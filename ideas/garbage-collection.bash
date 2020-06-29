61fc03e30cb3fb9c24f807ae43887ef34283ca01
Jeremy Banks <_@jeremy.ca>
2020-06-29 01:16:09 -0400

    m

---
+++ ideas/garbage-collection.bash
@@ -0,0 +1,8 @@
+: '
+  iterate over all declare assocative arrays ("objects")
+  and if they have no "references" (their own names/keys in other
+  variables, including other associative arrays and arrays), delete
+  them.
+  but maybe only if the have a generated name matching some form
+  corresponding to our allocator.
+'
