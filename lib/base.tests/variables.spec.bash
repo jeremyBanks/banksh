0713229aa727a1b007a9b9a2957caf3bb96fe3b2
Jeremy Banks <_@jeremy.ca>
2020-07-18 02:09:20 -0400

    add basic test for lib/base

---
+++ lib/base.tests/variables.spec.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shellcheck source=../../.banksh/lib/base
+source /dev/null && eval "$(
+  cat "$(dirname "$(realpath -- "$0")")/../../.banksh/lib/base" || echo exit 1)"
+
+[[ $BASH_COMPAT = 4.2 ]]
+[[ -d $__owd__ ]]
+[[ -f $__file__ ]]
+[[ $__dir__ ]]
+[[ $__name__ ]]
+[[ -f $__dir__/$__name__ ]]
+
+echo "enabled options (set & shopt):"
+(set -o && shopt) | grep -oP '^\S+(?=\s+on$)' | sort
